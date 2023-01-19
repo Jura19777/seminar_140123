16c6d80222c1899cc3cf0de5b08ee20d3d2f4b63
Jura19777 <poryadin77@gmail.com>
2023-01-19 21:40:36 +0700

    последнее число трехзначного - ответ

--- task4/Program.cs
+++ task4/Program.cs
@@ -7,8 +7,8 @@ if (a%2==1)
    a=a-1; 
 }
 
-Console.Write("Все четные числа от 1 до "+a+" будут: ");
-for (int i = 0; i < a+1; i+=2)
+Console.Write("Все четные числа от 1 до "+a+" будут:");
+for (int i = 2; i < a+1; i+=2)
     {
-        Console.Write(", "+i);
+        Console.Write(" "+i);
     }
---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse (Console.ReadLine());
+
+int b = a%100;
+int x = b%10;
+
+Console.Write("Последнее число "+a+" будет: "+x);
