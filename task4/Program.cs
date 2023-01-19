223e51434f7c05fc84ec5b690087fe430cda135f
Jura19777 <poryadin77@gmail.com>
2023-01-19 21:30:52 +0700

    все четные числа - ответ

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse (Console.ReadLine());
+
+if (a%2==1)
+{
+   a=a-1; 
+}
+
+Console.Write("Все четные числа от 1 до "+a+" будут: ");
+for (int i = 0; i < a+1; i+=2)
+    {
+        Console.Write(", "+i);
+    }
