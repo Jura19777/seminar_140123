8fc9d2e5bee8441ec81bbcfeb54bde9847ea450d
Jura19777 <poryadin77@gmail.com>
2023-01-19 21:51:10 +0700

    Вторая цифра трехзначного числа - ответ

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse (Console.ReadLine());
+
+int b = a%100;
+int x = b%10;
+
+Console.Write("Вторая цифра трехзначного числа "+a+" будет: "+(b-x)/10);
