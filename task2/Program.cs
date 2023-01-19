5f0113a2c3c3f209747510aaa62890ade9ca1da7
Jura19777 <poryadin77@gmail.com>
2023-01-19 20:54:30 +0700

    день недели - ответ

---
+++ task2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+Console.Write("Введите день недели [1..7]: ");
+int a = int.Parse (Console.ReadLine());
+
+if (a==1)
+{
+    Console.WriteLine("День недели - понедельник");
+}
+
+if (a==2)
+{
+    Console.WriteLine("День недели - вторник");
+}
+
+if (a==3)
+{
+    Console.WriteLine("День недели - среда");
+}
+
+if (a==4)
+{
+    Console.WriteLine("День недели - четверг");
+}
+
+if (a==5)
+{
+    Console.WriteLine("День недели - пятница");
+}
+
+if (a==6)
+{
+    Console.WriteLine("День недели - суббота");
+}
+
+if (a==7)
+{
+    Console.WriteLine("День недели - воскресенье");
+}
