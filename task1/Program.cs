59de87fb58d6539ccec9550be9a5671487b2e837
Jura19777 <poryadin77@gmail.com>
2023-01-19 20:45:54 +0700

    Первое число является квадратом второго числа - ответ

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.Clear;
+﻿Console.Clear();
 Console.Write("Введите первое число: ");
-int a = Console.ReadLine;
+int a = int.Parse (Console.ReadLine());
 Console.Write("Введите второе число: ");
-int b = Console.ReadLine;
+int b = int.Parse (Console.ReadLine());
 if (b*b==a)
 {
     Console.WriteLine("Первое число является квадратом второго числа");
