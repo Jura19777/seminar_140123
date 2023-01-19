2c813f26994ad35ce6f42d665aea3a0f8e1881f5
Jura19777 <poryadin77@gmail.com>
2023-01-19 22:06:30 +0700

    Показать наибольшую цифру числа - ответ

---
+++ task7/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+int x  = rnd.Next(10, 100);
+Console.WriteLine("Задано число: "+x);
+
+int s2 = x%10;
+int s1 = (x-s2)/10;
+
+if (s1>s2)
+{
+    Console.WriteLine("Число "+s1+" больше "+s2);
+}
+else
+{
+    Console.WriteLine("Число "+s2+" больше "+s1);
+}
