45035fb3288a6da6c0ec4f140475d53a46cedb8b
Jura19777 <poryadin77@gmail.com>
2023-01-20 19:11:59 +0700

    кратно ли число заданному - ответ

---
+++ task9/Program.cs
@@ -0,0 +1,15 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток
+Console.Clear();
+Console.Write("Введите число которое будем проверять на кратность: ");
+int a = int.Parse (Console.ReadLine());
+Console.Write("Введите число на которое будем делить: ");
+int b = int.Parse (Console.ReadLine());
+
+if (a%b==0)
+{
+    Console.WriteLine("Число "+a+" кратно числу "+b);
+}
+else
+{
+    Console.WriteLine("Число "+a+" некратно числу "+b+", остаток от деления будет "+a%b);
+}
