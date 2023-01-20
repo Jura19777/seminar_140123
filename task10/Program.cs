5097c931a3ff2b58058b9db77371b670fde22aa5
Jura19777 <poryadin77@gmail.com>
2023-01-20 19:19:34 +0700

    Найти третью цифру числа - ответ

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.Clear();
+Console.Write("Введите любое число: ");
+int a = int.Parse (Console.ReadLine());
+
+if (a/100==0)
+{
+    Console.WriteLine("Число "+a+", третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Число "+a+" третья цифра будет "+a/100);
+}
